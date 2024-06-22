617f7fec26fbc9d8c6db8e84be41fad772e5c07e
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-22 17:20:49 -0600

    chore: Update layout and add heading to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+  children, //This is the only parameter we are passing to the function, which is the children prop.
+}: Readonly<{ //This is a readonly type, which means that the object cannot be modified after it is created. : is used to specify the type of the object.
+  children: React.ReactNode; //This is the type of the children prop, which is a React node. React node is a type that can be a React element, a string, a number, or an array of React nodes.
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex p-12 justify-center`}>{children}</body>
     </html>
   );
 }
